b97c83c4068af3aff0f7fe16c9eb25b737665807
KaranShah1911 <karanshah19112005@gmail.com>
2025-07-14 15:45:51 +0530

    Doctor details done

--- backend/routes/doctor.routes.js
+++ backend/routes/doctor.routes.js
@@ -1,9 +1,29 @@
-import express from "express"
-import { doctorList, loginDoctor } from "../controllers/doctor.controllers.js";
+import express from "express";
+import {
+  doctorList,
+  loginDoctor,
+  appointmentsDoctor,
+  appointmentComplete,
+  appointmentCancel,
+  doctorDashboard,
+  doctorProfile,
+  updateDoctorProfile,
+} from "../controllers/doctor.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const doctorRouter = express.Router();
 
-doctorRouter.get('/list', doctorList)
-doctorRouter.post('login', loginDoctor)
+doctorRouter.get("/list", doctorList);
+doctorRouter.post("/login", loginDoctor);
 
-export default doctorRouter
+doctorRouter.get("/appointments", verifyJWT, appointmentsDoctor);
+
+doctorRouter.post("/complete-appointment", verifyJWT, appointmentComplete);
+doctorRouter.post("/cancel-appointment", verifyJWT, appointmentCancel);
+
+doctorRouter.get("/dashboard", verifyJWT, doctorDashboard);
+
+doctorRouter.get("profile", verifyJWT, doctorProfile);
+doctorRouter.post("/update-profile", verifyJWT, updateDoctorProfile);
+
+export default doctorRouter;
