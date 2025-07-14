6c0e9aa7087b877b0a4b16dd15c1d2123a6b49b9
KaranShah1911 <karanshah19112005@gmail.com>
2025-07-14 23:29:59 +0530

    Added password and email removal from doctoList

--- backend/controllers/doctor.controllers.js
+++ backend/controllers/doctor.controllers.js
@@ -25,7 +25,7 @@ const changeAvailibility = async (req, res) => {
 
 const doctorList = async (req, res) => { 
   try {
-    const doctors = await doctorModel.find({});
+    const doctors = await doctorModel.find({}).select(["-password", "-email"]);
 
     res.json({ success: true, doctors });
   } catch (error) {
