855f029f2c62511aae70dcdcd96006e79d14d713
DARSHIT SHAH <166908115+DARSHITSHAH-2906@users.noreply.github.com>
2025-07-18 01:25:00 +0530

    doctor controller changes

--- backend/controllers/doctor.controllers.js
+++ backend/controllers/doctor.controllers.js
@@ -8,7 +8,7 @@ import appointmentModel from "../models/appointment.models.js";
 
 const changeAvailibility = async (req, res) => {
   try {
-    const docId  = req.body.userId;
+    const {docId}  = req.body;
 
     const docData = await doctorModel.findById(docId);
     await doctorModel.findByIdAndUpdate(docId, {
