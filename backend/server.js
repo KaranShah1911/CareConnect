e51701408aad31f2b2929484661fd5aa8029ce41
DARSHIT SHAH <166908115+DARSHITSHAH-2906@users.noreply.github.com>
2025-07-20 14:36:40 +0530

    changed cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // middlewares
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['https://care-connect-black.vercel.app', 'https://care-connect-doctor.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
